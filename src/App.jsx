37838cf2fc8a6981e3bdfc1012a96b88002eec82
sumitdas <sumit260799@gmail.com>
2023-06-08 23:35:34 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,34 @@ function App() {
   const { loading, data } = Fetch(url);
   const [page, setPage] = useState(0);
   const [followers, setFollowers] = useState([]);
+
   useEffect(() => {
     if (loading) return;
     setFollowers(data[page]);
-  }, [loading]);
+  }, [loading, page]);
+
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+      return nextPage;
+    });
+  };
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1;
+      if (prevPage < 0) {
+        prevPage = data.length - 1;
+      }
+      return prevPage;
+    });
+  };
+
+  const handlePage = (index) => {
+    setPage(index);
+  };
 
   return (
     <main>
@@ -19,11 +43,33 @@ function App() {
         <div className="underline"></div>
       </div>
       <section className="followers">
-        {followers.map((follower) => {
-          return <Follower key={follower.id} {...follower} />;
-        })}
+        <div className="container">
+          {followers.map((follower) => {
+            return <Follower key={follower.id} {...follower} />;
+          })}
+        </div>
+        {!loading && (
+          <div className="btn-container">
+            <button className="prev-btn" onClick={prevPage}>
+              prev
+            </button>
+            {data.map((item, index) => {
+              return (
+                <button
+                  key={index}
+                  className={`page-btn ${index === page ? "active-btn" : null}`}
+                  onClick={() => handlePage(index)}
+                >
+                  {index + 1}
+                </button>
+              );
+            })}
+            <button className="next-btn" onClick={nextPage}>
+              next
+            </button>
+          </div>
+        )}
       </section>
-      hello
     </main>
   );
 }
--- src/Paginate.jsx
+++ src/Paginate.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Paginate(followers) {
-  const itemsPerPage = 10;
+  const itemsPerPage = 12;
   const pages = Math.ceil(followers.length / itemsPerPage);
   const newFollowers = Array.from({ length: pages }, (_, index) => {
     const start = index * itemsPerPage;
